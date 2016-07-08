765f586b7ce85401f62f9599ba24acdc52effeab
Philipp Gfeller <tuelsch@gmail.com>
2016-07-08 10:55:02 +0200

    Wow

--- build.js
+++ build.js
@@ -1,8 +1,24 @@
+require('shelljs/global');
 var webpack = require('webpack');
+var ora = require('ora');
 var config = require('./webpack.config');
+
+var spinner = ora('building for production...');
+spinner.start();
+
+rm('-rf', './dist');
+mkdir('-p', './dist');
 
 var compiler = webpack(config);
 
 compiler.run(function (err, stats) {
-  
+  spinner.stop();
+  if (err) throw err;
+  process.stdout.write(stats.toString({
+    colors: true,
+    modules: false,
+    children: false,
+    chunks: false,
+    chunkModules: false
+  }) + '\n');
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,79 @@
 var path = require('path');
 var glob = require('glob');
+var utils = require('./utils');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-var HtmlPlugins = glob.sync('./src/views/**/*.pug').map(function (entry) {
+var HtmlPlugins = glob.sync('./src/views/**/*.pug').map(function (view) {
   return new HtmlWebpackPlugin({
-    template: entry,
-    filename: path.win32.basename(entry, '.pug') + '.html'
+    template: view,
+    filename: path.win32.basename(view, '.pug') + '.html',
+    inject: true
   });
 });
 
+console.log(path.resolve(__dirname + '/src/js/main.js'));
+
 module.exports = {
   entry: {
+    app: './src/js/main.js'
   },
+
   output: {
     path: path.resolve(__dirname + '/dist'),
-    filename: '[name].js'
+    publicPath: '/',
+    filename: 'js/[name].js'
   },
+
   resolve: {
-    extensions: ['', '.js', '.pug']
+    extensions: ['', '.js', '.vue', '.pug'],
+    fallback: [path.join(__dirname, 'node_modules')],
+    alias: {
+      src: path.resolve(__dirname + '/src')
+    }
   },
+
+  /*externals: {
+    'vue': 'Vue'
+  },*/
+
   module: {
     loaders: [
       {
         test: /\.pug$/,
         loader: 'pug-static-loader'
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      },
+      {
+        test: /\.html$/,
+        loader: 'vue-html'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
       }
-    ]
+    ].concat(utils.styleLoaders())
   },
-  plugins: HtmlPlugins,
+
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new ExtractTextPlugin(utils.assetsPath('css/[name].css')),
+  ].concat(HtmlPlugins),
+
+  devtool: '#source-map',
+
+  vue: {
+    loaders: utils.cssLoaders({
+      sourceMap: '#source-map',
+      extract: true
+    })
+  },
+
   pug: {
     pretty: true,
     locals: {
